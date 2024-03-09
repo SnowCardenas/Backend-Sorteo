4b633c9014d4f2d5310361c62aa1fbf3a98444fa
Ricardo <soyricardodev@proton.me>
2024-03-09 18:03:21 +0000

    update connect to db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,4 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-async function connectToDatabase() {
-  try {
-    const client = await mongoose.connect(process.env.MONGO_URL);
-    console.log("DB Conectada");
-    return client;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 const client = mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Conectada"))
@@ -18,4 +6,4 @@ const client = mongoose
     console.log(error);
   });
 
-module.exports = { connectToDatabase, client };
+module.exports = client;
